4d46d05398008fe5aa848220fbb8c6c93756faf2
Sabrie Morales <sabriemorales1995@gmail.com>
2018-07-07 00:35:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+function getFirstSelector(selector){
+  //returns first element that matches
+  return document.querySelector(selector)
+}
 
+function nestedTarget(){
+  //return .target within #nested 
+  return document.querySelector(`#nested .target`);
+}
+function increaseRankBy(n){
+  let temp = document.querySelectorAll(`ul.ranked-list li`);
+  for(var i=0;i<temp.length;i++){
+    temp[i].innerHTML=parseInt(temp[i].innerHTML)+n;
+  }
+}
+
+function deepestChild(){
+  //returns the most deeply nested child from div#grand-node
+  /*
+  let list= document.querySelectorAll(`div#grand-node`);
+  let temp=list;
+  if(Array.isArray(list)){
+    for(var i=0; i<list.length;i++){
+    //  children.push(list[i]);
+    }
+  }*/
+ let child= document.querySelector(`div#grand-node`); 
+ while(child.querySelector(`div`)){
+   child= child.querySelector(`div`);
+ }
+ return child;
+}
+  
